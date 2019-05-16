f5ad8eac2194a7a661bbd04d32dadc273db05e56
Prince <princeradebe@gmail.com>
2019-05-16 19:58:36 +0200

    Added functionality to copy Audio and Document files into relevent folders

--- cleanMyDownloads.py
+++ cleanMyDownloads.py
@@ -1,35 +1,45 @@
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-Clean myDownloads is a script for moving files into a folder.
+Clean myDownloads is a script for moving files into folders.
 Grouped by extension
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 import os
+import shutil
+from pathlib import Path
 
+#-------------------------------------------------------------
+#downloadsFolder = r"C:\Users\radebep\Downloads"
+downloadsFolder = "C:/Users/radebep/Downloads/"
+#-------------------------------------------------------------
 audioPath       ='C:/Users/radebep/Downloads/Audio/'
-domumentsPath   ='C:/Users/radebep/Downloads/Documents/'
-softwarePath    ='C:/Users/radebep/Downloads/Softwares/'
+documentsPath   ='C:/Users/radebep/Downloads/Documents/'
+softwarePath    ='C:/Users/radebep/Downloads/Applications/'
 photosPath      ='C:/Users/radebep/Downloads/Photos/'
 compressedPath  ='C:/Users/radebep/Downloads/Compressed/'
-
-#Create folders, if not available to save files (Documents/Softwares/Audio/Photos/Compressed Files)
-#Only create foder for existing extensions
-#If the extension does not have a predefined folder name, create folder based on extension definition
+videoPath       ='C:/Users/radebep/Downloads/Video/'
+#-------------------------------------------------------------
+#Formats
+#audioF  = [".mp3",".mp4"]
+#Create folders, if not available to save files (Documents/Applications/Audio/Photos/Compressed Files)
+#Only create folder for existing extensions
+#If the extension does not have a predefined folder name, create folder based on extension definition (Future Feature)
 def createFolder(directory):
     try:
         if not os.path.exists(directory):   #Check folder availability, create if not available
             os.makedirs(directory)          #
     except OSError:
         print ('Error: Creating directory. ' +  directory + 'Folder already exist')
-        
-# Call creatFolder Function
-#createFolder()
-
-myFiles = os.listdir(r"C:\Users\radebep\Downloads")
-for files in myFiles:
-    print(files)
-
-
-
 
 #List out all files in the Downloads Folder
+myFiles = os.listdir(downloadsFolder)
+for files in myFiles:
+    #Copy all mp3 files to Audio Folder.
+    if ".mp3" in files and not os.path.exists(downloadsFolder + audioPath + files):
+        # Call creatFolder Function
+        createFolder(audioPath)
+        shutil.copy(downloadsFolder + files, audioPath)
 
-#Copy files to the relevent folder.
+    #Copy all doo files to Documents Folder.
+    elif ".doc" in files and not os.path.exists(downloadsFolder + documentsPath + files):
+        # Call creatFolder Function
+        createFolder(documentsPath)
+        shutil.copy(downloadsFolder + files, documentsPath)
