bd43c5ef1d776502a04d6c41e93b974f6d495a4d
Prince <princeradebe@gmail.com>
2019-05-16 21:49:04 +0200

    Added Lists for file formats.

--- cleanMyDownloads.py
+++ cleanMyDownloads.py
@@ -17,8 +17,13 @@ photosPath      ='C:/Users/radebep/Downloads/Photos/'
 compressedPath  ='C:/Users/radebep/Downloads/Compressed/'
 videoPath       ='C:/Users/radebep/Downloads/Video/'
 #-------------------------------------------------------------
-#Formats
-#audioF  = [".mp3",".mp4"]
+#File Formats
+audioFormat      = [".mp3",".mp4"]
+documentFormat   = [".doc",".docx",".pdf",".xls","xlsx",".ptt",".pttx",".txt"]
+softwareFormat   = [".exe"]
+photoFormat      = [".jpg",".jpeg",".png",".gif",".psd"]
+compressedFormat = [".zip",".rar"]
+videoFormat      = [".mp2",".mpeg4",".vid",".mkv"]
 #Create folders, if not available to save files (Documents/Applications/Audio/Photos/Compressed Files)
 #Only create folder for existing extensions
 #If the extension does not have a predefined folder name, create folder based on extension definition (Future Feature)
@@ -33,13 +38,38 @@ def createFolder(directory):
 myFiles = os.listdir(downloadsFolder)
 for files in myFiles:
     #Copy all mp3 files to Audio Folder.
-    if ".mp3" in files and not os.path.exists(downloadsFolder + audioPath + files):
+    name, ext = os.path.splitext(files)
+    if ext in audioFormat and not os.path.exists(downloadsFolder + audioPath + files):
         # Call creatFolder Function
         createFolder(audioPath)
         shutil.copy(downloadsFolder + files, audioPath)
 
     #Copy all doo files to Documents Folder.
-    elif ".doc" in files and not os.path.exists(downloadsFolder + documentsPath + files):
+    elif ext in documentFormat and not os.path.exists(downloadsFolder + documentsPath + files):
         # Call creatFolder Function
         createFolder(documentsPath)
         shutil.copy(downloadsFolder + files, documentsPath)
+
+    #Copy all Software files to Software Folder.
+    elif ext in softwareFormat and not os.path.exists(downloadsFolder + softwarePath + files):
+        # Call createFolder Function
+        createFolder(softwarePath)
+        shutil.copy(downloadsFolder + files, softwarePath)
+
+    #Copy all Photos to the Photos Folder
+    elif ext in photoFormat and not os.path.exists(downloadsFolder + photosPath + files):
+        #Call createFolder Function
+        createFolder(photosPath)
+        shutil.copy(downloadsFolder + files, photosPath)
+
+    #Copy all Compressed files to the Compressed Folder
+    elif ext in compressedFormat and not os.path.exists(downloadsFolder + compressedPath + files):
+        #Call createFolder Function
+        createFolder(compressedPath)
+        shutil.copy(downloadsFolder + files, compressedPath)
+
+    #Copy all Video files to the Compressed Folder
+    elif ext in videoFormat and not os.path.exists(downloadsFolder + videoPath + files):
+        #Call createFolder Function
+        createFolder(videoPath)
+        shutil.copy(downloadsFolder + files, videoPath)
